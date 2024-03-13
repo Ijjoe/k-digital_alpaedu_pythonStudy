3c147f2c6493866e64afe3b6b980c2696fb6ff27
Ijjoe <cijjj9220@gmail.com>
2024-03-13 09:46:02 +0900

    Create AI_파이썬_정리.mm
    마인드맵정리중

---
+++ AI_파이썬_정리.mm
@@ -0,0 +1,106 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="AI_파이썬_정리" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1710229526221"><hook NAME="MapStyle" background="#2e3440" zoom="0.769">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" followedTemplateLocation="template:/dark_nord_template.mm" followedMapLastTime="1670390084000" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="판다스" POSITION="right" ID="ID_1583717637" CREATED="1710230901697" MODIFIED="1710232112171">
+<node TEXT="pd.DataFrame(ndarray, index = [ ], column = [ ]) 기본형&#xa;&#xa;df = pd.DataFrame({&#xa;    &apos;name&apos; : [&apos;dive&apos;,&apos;idol&apos;,&apos;fire&apos;]&#xa;    ,&apos;sex&apos; : [&apos;M&apos;,&apos;F&apos;,&apos;M&apos;]&#xa;    ,&apos;age&apos;: [30,20,15]&#xa;},index=)" ID="ID_1873027471" CREATED="1710230910255" MODIFIED="1710231340191">
+<node TEXT="s=pd.read_csv(&quot;person_info.csv&quot;,encoding=&apos;euc-kr&apos;)" ID="ID_1494651885" CREATED="1710232919228" MODIFIED="1710232921066"/>
+<node TEXT="df.loc[[0,5],[&apos;이름&apos;,&apos;나이&apos;]]" ID="ID_1528154644" CREATED="1710233058316" MODIFIED="1710233060025"/>
+<node TEXT="df.iloc[[0,5],[-4]]" ID="ID_1321767949" CREATED="1710233213487" MODIFIED="1710233215176"/>
+<node TEXT="df.info()" ID="ID_1104640286" CREATED="1710233237245" MODIFIED="1710233238755">
+<node TEXT="&lt;class &apos;pandas.core.frame.DataFrame&apos;&gt;&#xa;RangeIndex: 8 entries, 0 to 7&#xa;Data columns (total 5 columns):&#xa; #   Column  Non-Null Count  Dtype&#xa;---  ------  --------------  -----&#xa; 0   이름      8 non-null      object&#xa; 1   출생일     8 non-null      object&#xa; 2   사망일     8 non-null      object&#xa; 3   나이      8 non-null      int64&#xa; 4   직업      8 non-null      object&#xa;dtypes: int64(1), object(4)&#xa;memory usage: 448.0+ bytes" ID="ID_1322481905" CREATED="1710233295573" MODIFIED="1710233300525"/>
+</node>
+<node TEXT="df[&apos;날짜일&apos;]=pd.to_datetime(df[&apos;출생일&apos;])" ID="ID_1902584877" CREATED="1710233535080" MODIFIED="1710233537122"/>
+</node>
+<node TEXT="pd.Series([11,22,33])" ID="ID_674224008" CREATED="1710230989737" MODIFIED="1710231018764">
+<node TEXT="열을 쉽게 생성해주는 메서드" ID="ID_372166325" CREATED="1710231019892" MODIFIED="1710231041423"/>
+<node TEXT=".values&#xa;---------------------------&#xa;.index 특정값 바로접근 NO 다음행적용하거나 변수담아야됨&#xa;ex) data.index&#xa;      data[1]&#xa;--------------------------" ID="ID_1514415308" CREATED="1710231142557" MODIFIED="1710231286030">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+<node TEXT="• Groupby 함수 소개&#xa;통계 함수&#xa;count&#xa;size&#xa;mean&#xa;std&#xa;min&#xa;max&#xa;sum&#xa;var&#xa;describe&#xa;first&#xa;last&#xa;&#xa;표준편차&#xa;최소값&#xa;최대값&#xa;전체 합&#xa;분산&#xa;요약 통계량&#xa;첫번째 행&#xa;마지마 해&#xa;설명&#xa;누락값을 제외한 데이터 수&#xa;누락값을" ID="ID_930129543" CREATED="1710231681807" MODIFIED="1710232112170" VSHIFT_QUANTITY="-0.75 pt"/>
+<node TEXT="그래프 그리기 함수&#xa;.plot()" ID="ID_423691322" CREATED="1710232104978" MODIFIED="1710232127768"/>
+<node TEXT=".groupby([])[]" ID="ID_525746932" CREATED="1710232179451" MODIFIED="1710232500364">
+<node TEXT="bb=df.groupby(&quot;continent&quot;)[&apos;country&apos;].count()" ID="ID_916175432" CREATED="1710232480342" MODIFIED="1710232485308"/>
+<node TEXT=".groupby([])[] .mean()" ID="ID_1862433604" CREATED="1710232492689" MODIFIED="1710232494525"/>
+<node TEXT=".nunique()" ID="ID_272791181" CREATED="1710232507992" MODIFIED="1710232918427"/>
+</node>
+</node>
+<node TEXT=" 여러줄 주석 : 큰따옴표 3개, 작은따옴표 3개" POSITION="left" ID="ID_1679602396" CREATED="1710231471952" MODIFIED="1710231474909"/>
+</node>
+</map>
