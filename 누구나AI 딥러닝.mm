72b9eb068e2135af380cfe21084f3ebd8b491395
Ijjoe <cijjj9220@gmail.com>
2024-03-14 14:20:32 +0900

    AI_자료정리
    마인드맵 정리

---
+++ 누구나AI 딥러닝.mm
@@ -0,0 +1,119 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="누구나AI 딥러닝" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1708914994451"><hook NAME="MapStyle" background="#2e3440">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="1차_OT" POSITION="bottom_or_right" ID="ID_51477999" CREATED="1708914996181" MODIFIED="1708915001028">
+<node TEXT="github 실습" ID="ID_1919120807" CREATED="1708915002588" MODIFIED="1708915019586">
+<node TEXT="github Desktop" ID="ID_1226344179" CREATED="1708915049813" MODIFIED="1708915068371"/>
+<node TEXT="https://github.com/Ijjoe/Ijjoe.github.io.git" ID="ID_853111162" CREATED="1708915114157" MODIFIED="1708915115822"/>
+<node TEXT="Create a New 레포지토리" ID="ID_335831881" CREATED="1708915219675" MODIFIED="1708915241429"/>
+<node TEXT="test레포" ID="ID_1616270885" CREATED="1708915420356" MODIFIED="1708915428818">
+<node TEXT="https://github.com/Ijjoe/localRep" POSITION="bottom_or_right" ID="ID_464351717" CREATED="1708915413311" MODIFIED="1708915415454"/>
+<node TEXT="https://github.com/Ijjoe/Ijjoe" ID="ID_960635411" CREATED="1708915834311" MODIFIED="1708916019635">
+<node TEXT="자기소개" ID="ID_1405402721" CREATED="1708915839904" MODIFIED="1708915859761"/>
+<node TEXT="자신의 ID로 해야 프로필 생성됨" ID="ID_1023443261" CREATED="1708916204944" MODIFIED="1708916326710"/>
+</node>
+</node>
+<node TEXT="은어 __ git 잔디" ID="ID_1767272259" CREATED="1708915612590" MODIFIED="1708915623069"/>
+<node TEXT="https://github.com/shiny0510" ID="ID_981282579" CREATED="1708916330218" MODIFIED="1708916332572">
+<node TEXT="https://github.com/shiny0510/README_example_Source" ID="ID_895050433" CREATED="1708916401834" MODIFIED="1708916404773"/>
+</node>
+<node TEXT="fork" ID="ID_1054839026" CREATED="1708916509478" MODIFIED="1708916517243">
+<node TEXT="https://github.com/Ijjoe/README_example_Source" ID="ID_1517459166" CREATED="1708916518836" MODIFIED="1708916523108"/>
+</node>
+</node>
+<node TEXT="python_study폴더생성" ID="ID_1544201829" CREATED="1708917277214" MODIFIED="1708917381494">
+<node TEXT="※※ 온라인강좌 시작 후  매일코딩 push작업할것" ID="ID_1411714224" CREATED="1708917294038" MODIFIED="1708917491716" BACKGROUND_COLOR="#ff3399"/>
+</node>
+<node TEXT="https://school.programmers.co.kr/learn/challenges?order=recent" ID="ID_388970069" CREATED="1708917759621" MODIFIED="1708917761539">
+<node TEXT="코딩문제은행" ID="ID_1415742526" CREATED="1708917764445" MODIFIED="1708917779601"/>
+</node>
+</node>
+<node TEXT="Offline1차수업" POSITION="top_or_left" ID="ID_765409290" CREATED="1710375658377" MODIFIED="1710375676872">
+<node TEXT="대회정보" ID="ID_664591970" CREATED="1710376895602" MODIFIED="1710376905993">
+<node TEXT="kaggle, dacon 등 머신러닝 competition" POSITION="top_or_left" ID="ID_409435915" CREATED="1710376816827" MODIFIED="1710376931463">
+<node TEXT="Dacon이라는 Ai경진대회 플랫폼에서 진행한 대회 저장소입니다." ID="ID_1029734673" CREATED="1710376885574" MODIFIED="1710376887438"/>
+<node TEXT="Kaggle에서 진행한 대회 저장소입니다." ID="ID_736311513" CREATED="1710376892800" MODIFIED="1710376894428"/>
+</node>
+</node>
+<node TEXT="리,튜 == []() 순서있음" ID="ID_759666286" CREATED="1710384170263" MODIFIED="1710384197020"/>
+<node TEXT="from collections import deque&#xa;#https://dongdongfather.tistory.com/72  &#xa;# deque 사용법&#xa;&#xa;dq = deque(&apos;love&apos;)&#xa;&#xa;dq.appendleft(&apos;m&apos;)&#xa;dq.extend(&apos; you&apos;)" ID="ID_58873991" CREATED="1710393552397" MODIFIED="1710393571918"/>
+</node>
+</node>
+</map>
