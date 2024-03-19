909d6f7d3d4aa95583e59754474099007be02d33
Ijjoe <cijjj9220@gmail.com>
2024-03-19 11:07:37 +0900

    Update 누구나AI 딥러닝.mm
    ofline_4차시

--- 누구나AI 딥러닝.mm
+++ 누구나AI 딥러닝.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.11.5">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="누구나AI 딥러닝" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1708914994451"><hook NAME="MapStyle" background="#2e3440">
+<node TEXT="누구나AI 딥러닝" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1708914994451"><hook NAME="MapStyle" background="#2e3440" zoom="0.826">
     <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" fit_to_viewport="false"/>
 
 <map_styles>
@@ -105,7 +105,7 @@
 <node TEXT="코딩문제은행" ID="ID_1415742526" CREATED="1708917764445" MODIFIED="1708917779601"/>
 </node>
 </node>
-<node TEXT="Offline1차수업" POSITION="top_or_left" ID="ID_765409290" CREATED="1710375658377" MODIFIED="1710375676872">
+<node TEXT="Offline1차수업" FOLDED="true" POSITION="top_or_left" ID="ID_765409290" CREATED="1710375658377" MODIFIED="1710375676872">
 <node TEXT="대회정보" ID="ID_664591970" CREATED="1710376895602" MODIFIED="1710376905993">
 <node TEXT="kaggle, dacon 등 머신러닝 competition" POSITION="top_or_left" ID="ID_409435915" CREATED="1710376816827" MODIFIED="1710376931463">
 <node TEXT="Dacon이라는 Ai경진대회 플랫폼에서 진행한 대회 저장소입니다." ID="ID_1029734673" CREATED="1710376885574" MODIFIED="1710376887438"/>
@@ -114,6 +114,43 @@
 </node>
 <node TEXT="리,튜 == []() 순서있음" ID="ID_759666286" CREATED="1710384170263" MODIFIED="1710384197020"/>
 <node TEXT="from collections import deque&#xa;#https://dongdongfather.tistory.com/72  &#xa;# deque 사용법&#xa;&#xa;dq = deque(&apos;love&apos;)&#xa;&#xa;dq.appendleft(&apos;m&apos;)&#xa;dq.extend(&apos; you&apos;)" ID="ID_58873991" CREATED="1710393552397" MODIFIED="1710393571918"/>
+<node TEXT="# 리스트 컴프리헨션&#xa;&apos;&apos;&apos;&#xa;matrix = [&#xa;    [1, 2, 3, 4],&#xa;    [5, 6, 7, 8],&#xa;    [9, 10, 11, 12],&#xa;]&#xa;&apos;&apos;&apos;&#xa;#2차원 배열로 가능 함&#xa;co=[(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]&#xa;#[(1, 3), (1, 4), (2, 3), (2, 1), (2, 4), (3, 1), (3, 4)]&#xa;&#xa;&#xa;#3차원 배열&#xa;list(zip(*matrix))&#xa;[(1, 5, 9), (2, 6, 10), (3, 7, 11), (4, 8, 12)]" ID="ID_347926191" CREATED="1710393924102" MODIFIED="1710394056675"/>
+<node TEXT="배열 빈값 제거&#xa;&#xa;def solution(my_string):&#xa;    ccc = my_string.split(&apos; &apos;)&#xa;    answer = [x for x in ccc if x]&#xa;&#xa;    return answer" ID="ID_883249833" CREATED="1710397200762" MODIFIED="1710397215384"/>
 </node>
+<node TEXT="Offline2차수업" POSITION="top_or_left" ID="ID_214760333" CREATED="1710463293417" MODIFIED="1710481618883">
+<node TEXT="ai데이터 공유모음" ID="ID_1717225462" CREATED="1710463319347" MODIFIED="1710463515950">
+<node TEXT="https://www.aihub.or.kr/&#xa;케글" ID="ID_1543596738" CREATED="1710463344285" MODIFIED="1710463351024"/>
+</node>
+<node TEXT="https://www.crummy.com/software/BeautifulSoup/bs4/doc/&#xa;&#xa;https://requests.readthedocs.io/en/latest/" ID="ID_102023353" CREATED="1710476047163" MODIFIED="1710476370580">
+<node TEXT="import requests&#xa;from bs4 import BeautifulSoup" POSITION="top_or_left" ID="ID_724453081" CREATED="1710476075988" MODIFIED="1710476078458"/>
+</node>
+<node TEXT="00_웹크롤링 맛보기.ipynb" ID="ID_1687785254" CREATED="1710481682397" MODIFIED="1710481685539">
+<node TEXT="https://developer.mozilla.org/en-US/docs/Web/CSS/:nth-of-type" ID="ID_49920090" CREATED="1710489921268" MODIFIED="1710489923018">
+<node TEXT="CSS관련 정보 카운팅부분참조" ID="ID_910411519" CREATED="1710489928881" MODIFIED="1710489960735"/>
+</node>
+</node>
+</node>
+<node TEXT="정규표현식" POSITION="bottom_or_right" ID="ID_240014075" CREATED="1710464970576" MODIFIED="1710464980346">
+<node TEXT="Python에서는 정규표현 처리를 하기 위해 표준 라이브러리인 re 모듈을 사용한다. 정규표현 패턴를 이용한 문자열의 추출이나, 치환, 분할 등이 가능하다. &#xa; 이번 포스팅에서는 먼저 re모듈의 함수나 메소드에 대해서 설명한다.&#xa;- 정규 표현 패턴의 컴파일 compile()&#xa;- 매치 오브젝트&#xa;- 문자열의 앞 부분이 매치되는가를 체크, 추출 match()&#xa;- 선두에 한해서 매치하는지를 체크, 추출 search()&#xa;- 문자열 전부가 매치되는가를 체크 fullmatch()&#xa;- 매치된 부분 모두 리스트로 취득 findall()&#xa;- 매치된 부분 모두 이터레이터(iterator)로 취득 finditer()&#xa;- 매치된 부분을 치환 sub() subn()&#xa;- 정규표현 패턴으로 문자열을 분할 spilt()&#xa; 그 다음에는 re모듈로 사용할 수 있는 정규표현의 패턴 문자(특수 문자), 특수 시퀀스에 대해서 설명한다. 기본적으로 표준적으로 정규표현의 구문(신택스)이지만, 플래그의 설정 (특히, re.ASCII)는 주의할 필요가 있다.&#xa;- Python에서의 정규표현의 메타 문자, 특수 시퀀스와 주의점&#xa;- 플래그의 설정&#xa;&#xa;ASCII문자열에 한정 re.ASCII&#xa;대문자소문자를 구별하지 않는다 re.IGNORECASE&#xa;각행의 선두, 말미에 매치 re.MULTILINE&#xa;여러 개의 플래그를 설정&#xa;&#xa;- greedy match 와 비(非)greedy match&#xa;출처: https://engineer-mole.tistory.com/189" ID="ID_1230574215" CREATED="1710464982678" MODIFIED="1710465535063">
+<node TEXT="매치된 위치를 취득 start(), end(), span()&#xa;매치된 문자열을 취득 group()&#xa;각 그룹의 문자열을 취득 groups()&#xa;&#xa;매치된 부분의 위치는 start(), end(), span(), 문자열은 group().&#xa;출처: https://engineer-mole.tistory.com/189 [매일 꾸준히, 더 깊이:티스토리]" ID="ID_1724058809" CREATED="1710465674374" MODIFIED="1710465676485"/>
+<node TEXT="https://nachwon.github.io/regular-expressions/" ID="ID_1870588105" CREATED="1710734879390" MODIFIED="1710734883426"/>
+</node>
+</node>
+<node TEXT="Offline3차수업" POSITION="top_or_left" ID="ID_360692853" CREATED="1710734155850" MODIFIED="1710734179754">
+<node TEXT="코랩 파일 구글 드라이브 이동명령어" ID="ID_662798833" CREATED="1710737473816" MODIFIED="1710737489327">
+<node TEXT="!mv /content/종합_top50 /content/drive/My\ Drive/원하는경로/" ID="ID_427897930" CREATED="1710737491678" MODIFIED="1710737493435"/>
+<node TEXT="!cp -rp /content/종합_top50 /content/drive/MyDrive/AI_jupter_TEST/종합_top50" ID="ID_776820145" CREATED="1710737782591" MODIFIED="1710737785059"/>
+<node TEXT="from google.colab import drive&#xa;drive.mount(&apos;/content/drive&apos;)" ID="ID_795560189" CREATED="1710737911740" MODIFIED="1710737914370"/>
+</node>
+<node TEXT="enumerate  인덱스 부여하는 함수" ID="ID_1670485241" CREATED="1710740226644" MODIFIED="1710740246513"/>
+<node TEXT="iframe" ID="ID_1180062656" CREATED="1710743212876" MODIFIED="1710743222959"/>
+<node TEXT="bs4.element.Tag" ID="ID_963782109" CREATED="1710747999302" MODIFIED="1710748001293"/>
+</node>
+<node TEXT="Offline4차수업" POSITION="top_or_left" ID="ID_251058977" CREATED="1710734155850" MODIFIED="1710734197263">
+<node TEXT="with open(&apos;/content/cospi200/cosip2001.csv&apos;,&apos;a&apos;) as f:&#xa;    f.write(df_ksp200.to_csv())&#xa;&#xa;&#xa;f.close()" ID="ID_1943626599" CREATED="1710811807027" MODIFIED="1710811820881">
+<node TEXT="df_ksp200.to_csv(&apos;코스피200_일별시세.csv&apos;, encoding=&apos;utf-8-sig&apos;, index=False)" ID="ID_1252356509" CREATED="1710812871650" MODIFIED="1710812874248"/>
+</node>
+</node>
+<node TEXT="Offline5차수업" POSITION="top_or_left" ID="ID_764150158" CREATED="1710734155850" MODIFIED="1710734198906"/>
 </node>
 </map>
